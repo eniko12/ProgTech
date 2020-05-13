e89df76e543bcd3178710a26dfd7f8cf71152586
Regina <urbanregina12@gmail.com>
2020-05-13 15:02:50 +0200

    Arany és pont hiánya kijavítva

--- GamifyYourLife/src/gamifyyourlife/View/CreateNewTaskView.java
+++ GamifyYourLife/src/gamifyyourlife/View/CreateNewTaskView.java
@@ -16,16 +16,27 @@ public class CreateNewTaskView {
         System.out.println("----------------------------------------");
         System.out.println("Új feladat hozzáadása");
         System.out.print("Feladat neve: ");
-        this.newTaskName = in.nextLine();
+        this.newTaskName = in.nextLine();        
         System.out.println("Adja meg a feladat típusát!");
         System.out.print("Munka - j, Hobbi - h, egyéb - o: ");
         this.newTaskType = in.nextLine();
         if(this.newTaskType.toLowerCase().equals("j") || this.newTaskType.toLowerCase().equals("h") || this.newTaskType.toLowerCase().equals("o") ){
             System.out.print("Arany jutalom: ");
+            try{
             this.newTaskGold = Integer.parseInt(in.nextLine());
+            }catch(NumberFormatException e){
+                 System.out.println("Kötelező mező! Az érték autómatikusan 0-ra állítva.");
+                 this.newTaskGold = 0;
+            }
             System.out.print("Pont jutalom: ");
+            try{
             this.newTaskPoint = Integer.parseInt(in.nextLine());
             System.out.println();
+            }
+            catch(NumberFormatException e){
+                 System.out.println("Kötelező mező! Az érték autómatikusan 0-ra állítva.");
+                 this.newTaskPoint = 0;
+            }
             System.out.println("----------------------------------------"); 
         }
         else{
